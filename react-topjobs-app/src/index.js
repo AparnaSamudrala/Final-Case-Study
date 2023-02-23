3490c82168b1598b9fb06d58a6f6726aae892613
Aparna Samudrala <67942273+AparnaSamudrala@users.noreply.github.com>
2023-02-24 01:06:19 +0530

    Update index.js

--- react-topjobs-app/src/index.js
+++ react-topjobs-app/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -9,9 +9,9 @@ import "../node_modules/font-awesome/css/font-awesome.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
